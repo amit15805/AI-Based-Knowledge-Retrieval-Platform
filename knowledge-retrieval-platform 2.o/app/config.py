f3cb8061fcd7caf48408a23de41301f57ef36731
Amitkumar_15th <191383055+amit15805@users.noreply.github.com>
2026-09-22 20:43:45 +0530

    Update config.py

--- knowledge-retrieval-platform 2.o/app/config.py
+++ knowledge-retrieval-platform 2.o/app/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Settings(BaseModel):
     APP_NAME: str = "AI Knowledge Retrieval & Query Resolution Platform"
-    GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY", "AQ.Ab8RN6IaLc_uT7VzSKoCi8eWW2rLjRtJdHYS-wE8gmKGsxXjgA")
+    GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY", "")
     EMBEDDING_MODEL: str = "sentence-transformers/all-MiniLM-L6-v2"
     SIMILARITY_THRESHOLD: float = 0.45
     PORT: int = 8000
